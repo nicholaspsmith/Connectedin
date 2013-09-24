985ac0aa1caede7a0cca1605c000e8cf4e04ae04
Nicholas P Smith <npsmith1990@gmail.com>
2013-09-24 17:14:04 -0500

    Complete exercises

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
 
   def show_all
+    @all = [show_harsh, show_nicole, show_yasmin]
   end
 
   def show_harsh
@@ -8,25 +9,28 @@ class StudentsController < ApplicationController
         name: "Harsh Patel",
         city: "St. Louis",
         description: "The Indian man with glasses",
-        picture_url: "http://goo.gl/GwwMqe"
+        picture_url: "http://goo.gl/GwwMqe",
+        path: harsh_path
     }
   end
 
-  def show_mike
+  def show_nicole
     @student = {
-        name: "Harsh Patel",
-        city: "St. Louis",
-        description: "The Indian man with glasses",
-        picture_url: "http://goo.gl/GwwMqe"
+        name: "Nicole Pank",
+        city: "Jersey Shore",
+        description: "Actually North Jersey",
+        picture_url: "https://scontent-b-dfw.xx.fbcdn.net/hphotos-ash3/1174903_828444650107_128913837_n.jpg",
+        path: nicole_path
     }
   end
 
-  def show_shehzan
+  def show_yasmin
     @student = {
-        name: "Harsh Patel",
-        city: "St. Louis",
-        description: "The Indian man with glasses",
-        picture_url: "http://goo.gl/GwwMqe"
+        name: "Yasmin Fazelinia",
+        city: "Austin",
+        description: "I dont know....",
+        picture_url: "https://scontent-b-dfw.xx.fbcdn.net/hphotos-frc3/1904_10102889709685500_129841736_n.jpg",
+        path: yasmin_path
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Connectedin::Application.routes.draw do
-  root "students#show_all"
-  get "/students" => "students#show_harsh", as: 'harsh'
+  root "students#show_all", as: 'home'
+  get "/students/show_harsh" => "students#show_harsh", as: 'harsh'
+  get "/harsh" => "students#show_harsh"
+  get "/students/show_nicole" => "students#show_nicole", as: 'nicole'
+  get "/nicole" => "students#show_nicole"
+  get "/students/show_yasmin" => "students#show_yasmin", as: 'yasmin'
+  get "/yasmin" => "students#show_yasmin"
+  get "/404" => "error_pages#404", as: 'four_oh_four'
 end
